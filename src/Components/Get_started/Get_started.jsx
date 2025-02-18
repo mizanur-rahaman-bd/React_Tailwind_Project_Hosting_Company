53efa03bec801c8295a2fba119877ca9042a9b08
mizanur-rahaman-bd <sumon1581@gmail.com>
2025-02-18 20:25:08 +0600

    onk_din_por

--- src/Components/Get_started/Get_started.jsx
+++ src/Components/Get_started/Get_started.jsx
@@ -32,77 +32,77 @@ const Get_started = () => {
             <div className="basic_span w-[444px]">
               <h2>Basic</h2>
               <span className="w-[444px] h-[2px] bg-[#C4C4C4] block"></span>
-              <div className="span_card_row">
-                <div className="span_card_inner_row">
-                  <div className="span_single_card">
-                    <h3>Starter</h3>
-                    <p>
-                      with all your customers via all conversation channels in
-                      one central dashboard.
-                    </p>
-                    <h4>$2.80</h4>
-                    <h6>Per month</h6>
-                    <button>Choose this Plan</button>
-                    <ul>
-                      <li>2GB SSD</li>
-                      <li>10GB Bandwidth</li>
-                      <li>15 Email Accounts</li>
-                      <li>Unlimited Database</li>
-                      <li>4 Subdomains</li>
-                      <li>1 Parked Domain</li>
-                      <li>2 Websites</li>
-                      <li>Free SSL</li>
-                      <li>Softaculous</li>
-                    </ul>
-                  </div>
-                  <div className="span_single_card">
-                    <h3>Starter</h3>
-                    <p>
-                      with all your customers via all conversation channels in
-                      one central dashboard.
-                    </p>
-                    <h4>$2.80</h4>
-                    <h6>Per month</h6>
-                    <button>Choose this Plan</button>
-                    <ul>
-                      <li>2GB SSD</li>
-                      <li>10GB Bandwidth</li>
-                      <li>15 Email Accounts</li>
-                      <li>Unlimited Database</li>
-                      <li>4 Subdomains</li>
-                      <li>1 Parked Domain</li>
-                      <li>2 Websites</li>
-                      <li>Free SSL</li>
-                      <li>Softaculous</li>
-                    </ul>
-                  </div>
-                  <div className="span_single_card">
-                    <h3>Starter</h3>
-                    <p>
-                      with all your customers via all conversation channels in
-                      one central dashboard.
-                    </p>
-                    <h4>$2.80</h4>
-                    <h6>Per month</h6>
-                    <button>Choose this Plan</button>
-                    <ul>
-                      <li>2GB SSD</li>
-                      <li>10GB Bandwidth</li>
-                      <li>15 Email Accounts</li>
-                      <li>Unlimited Database</li>
-                      <li>4 Subdomains</li>
-                      <li>1 Parked Domain</li>
-                      <li>2 Websites</li>
-                      <li>Free SSL</li>
-                      <li>Softaculous</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
             </div>
             <div className="premium_span w-[444px]">
               <h2>Premium</h2>
               <span className="w-[444px] h-[2px] bg-[#c4c4c4] block"></span>
+            </div>
+            <div className="span_card_row">
+              <div className="span_card_inner_row">
+                <div className="span_single_card">
+                  <h3>Starter</h3>
+                  <p>
+                    with all your customers via all conversation channels in one
+                    central dashboard.
+                  </p>
+                  <h4>$2.80</h4>
+                  <h6>Per month</h6>
+                  <button>Choose this Plan</button>
+                  <ul>
+                    <li>2GB SSD</li>
+                    <li>10GB Bandwidth</li>
+                    <li>15 Email Accounts</li>
+                    <li>Unlimited Database</li>
+                    <li>4 Subdomains</li>
+                    <li>1 Parked Domain</li>
+                    <li>2 Websites</li>
+                    <li>Free SSL</li>
+                    <li>Softaculous</li>
+                  </ul>
+                </div>
+                <div className="span_single_card">
+                  <h3>Starter</h3>
+                  <p>
+                    with all your customers via all conversation channels in one
+                    central dashboard.
+                  </p>
+                  <h4>$2.80</h4>
+                  <h6>Per month</h6>
+                  <button>Choose this Plan</button>
+                  <ul>
+                    <li>2GB SSD</li>
+                    <li>10GB Bandwidth</li>
+                    <li>15 Email Accounts</li>
+                    <li>Unlimited Database</li>
+                    <li>4 Subdomains</li>
+                    <li>1 Parked Domain</li>
+                    <li>2 Websites</li>
+                    <li>Free SSL</li>
+                    <li>Softaculous</li>
+                  </ul>
+                </div>
+                <div className="span_single_card">
+                  <h3>Starter</h3>
+                  <p>
+                    with all your customers via all conversation channels in one
+                    central dashboard.
+                  </p>
+                  <h4>$2.80</h4>
+                  <h6>Per month</h6>
+                  <button>Choose this Plan</button>
+                  <ul>
+                    <li>2GB SSD</li>
+                    <li>10GB Bandwidth</li>
+                    <li>15 Email Accounts</li>
+                    <li>Unlimited Database</li>
+                    <li>4 Subdomains</li>
+                    <li>1 Parked Domain</li>
+                    <li>2 Websites</li>
+                    <li>Free SSL</li>
+                    <li>Softaculous</li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
